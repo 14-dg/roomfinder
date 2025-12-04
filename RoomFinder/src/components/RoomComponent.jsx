2d1a66fd4cb93930b1e4a91c88639f34509638a6
eliebrec <erik.liebrecht@smail.th-koeln.de>
2025-12-04 12:32:14 +0100

    rooms+roomcomponent

---
+++ RoomFinder/src/components/RoomComponent.jsx
@@ -0,0 +1,18 @@
+//anzeige von räumen mit ihren eigenschaften in boxen in Rooms.jsx als liste
+export default function RoomComponent({
+  roomNumber = '101',
+  floor = '1',
+  availability = 'Available',
+  seats = '25',
+  equipment = 'Projector, Whiteboard',
+}) {
+  return (
+    <div className="room-box">
+      <h4>Room {roomNumber}</h4>
+      <p>Floor: {floor}</p>
+      <p>Availability: {availability}</p>
+      <p>Seats: {seats}</p>
+      <p>Equipment: {equipment}</p>
+    </div>
+  )
+}
