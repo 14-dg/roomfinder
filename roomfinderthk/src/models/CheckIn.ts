34b7e0d0d61116b54b78eb3a269af0ee80586a21
Kai Siepmann <kai_leo_theodor.siepmann@smail.th-koeln.de>
2025-12-14 22:46:02 +0100

    Added CheckIn model

---
+++ roomfinderthk/src/models/CheckIn.ts
@@ -0,0 +1,10 @@
+export interface CheckIn {
+    id: string;
+    roomId: string;
+    day: string;
+    timeSlot: string;
+    studentId: string;
+    studentName: string;
+    activity: string;
+    createdAt: Date;
+}
