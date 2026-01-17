b1077a52388d6265d51d3b38883b498bc1fd3073
Michael Liborius Beckmann <michael_liborius.beckmann@smail.th-koeln.de>
2026-01-17 21:29:04 +0100

    some changes for Timetabel Builder

--- roomfinderthk/src/screens/admin/timetablebuilder/TimetableBuilder.tsx
+++ roomfinderthk/src/screens/admin/timetablebuilder/TimetableBuilder.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { Switch } from '@/components/ui/switch';
+import { Label } from '@/components/ui/label';
 import './timetablebuilder.css'
+import {
+    setEventTimetableBuilder,
+    getEventsTimetableBuilder
+} from '@/services/firebase';
+import { Lecture } from '@/models';
 
 interface TimetableBuilderProps {
     courseOfStudy: string;
@@ -7,28 +14,83 @@ interface TimetableBuilderProps {
     year: number;
 }
 
+interface Timetable {
+    days: string[];
+    events: Event[];
+}
+
+interface Event {
+    id: number;
+    day: string;
+    startTime: string;
+    endTime: string;
+    name: string;
+    lecturer: Lecturer;
+    room: Room;
+    module: Module;
+    type: string;
+}
+
+interface Lecturer {
+    id: string;
+    name: string;
+    email: string;
+    officeHours: string;
+    events: Event[];
+}
+
+interface Room {
+    id: number;
+    roomNumber: string;
+    floor: number;
+    capacity: number;
+    occupiedSeats: number;
+    hasBeamer: boolean;
+    isLocked: boolean;
+}
+
+interface Module {
+    id: number
+    name: string;
+}
+
 const TimetableBuilder: React.FC<TimetableBuilderProps> = ({ courseOfStudy, semester, year }) => {
-    const days = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag'];
-    const timeSlots: string[] = [];
-    let startHour = 8;
-    let startMinute = 0;
-    for (let i = 0; i < 14; i++) {
-        const duration = i % 2 === 0 ? 50 : 55;
-        const startStr = `${String(startHour).padStart(2, '0')}:${String(startMinute).padStart(2, '0')}`;
-        timeSlots.push(startStr);
-        const endTime = new Date(2023, 0, 1, startHour, startMinute + duration);
-        startHour = endTime.getHours();
-        startMinute = endTime.getMinutes();
-    }
+    const days: string[] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+    const timeSlots: string[] = ['08:00', '08:50', '09:45', '10:35', '11:30', '12:20', '13:15', '14:05', '15:00', '15:50', '16:45', '17:35', '18:30', '19:20'];
+    const endDay: string = '20:15';
+
+    // const [events, setEventTimetableBuilder] = useState<Event[]>([]);
+
+    const [events, setEvents] = useState();
+    const [rooms, setRooms] = useState();
+    const [lecturers, setLecturers] = useState();
+    const [modules, setModules] = useState();
+
+    const [showForm, setShowForm] = useState(false);
+    const [formData, setFormData] = useState({ day: '', time: 0, event: null });
+    const [contextMenu, setContextMenu] = useState(null);
+    const contextMenuRef = useRef(null);
+    const timetableRef = useRef(null);
 
 
-    const handleTimeSlotClick = (day: string, hour: string) => {
-        alert(`Tag: ${day}, Uhrzeit: ${hour}`);
+    const handleTimeSlotClick = (day: string, startTime: string) => {
+        alert(`Tag: ${day}, Uhrzeit: ${startTime}`);
     };
 
     return (
         <>
             <h3 className="text-lg font-semibold">Timetable Builder: {courseOfStudy}, {semester} Semester, {year}</h3>
+
+            {/* <div className="flex items-center gap-2">
+              <Switch
+                id="hasBeamer"
+                checked={newRoom.hasBeamer}
+                onCheckedChange={(checked) =>
+                  setNewRoom({ ...newRoom, hasBeamer: checked })
+                }
+              />
+              <Label htmlFor="hasBeamer">Has Beamer</Label>
+            </div> */}
 
             <table className="timetable">
                 <thead>
