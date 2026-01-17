a4340a125baaa1cbf0ffdbbff90efeae9afca1b1
Kai Siepmann <kai_leo_theodor.siepmann@smail.th-koeln.de>
2026-01-17 15:51:37 +0100

    occupancy hilfsfunktionen angepasst und um typ OccupancyLevel erweitert

--- roomfinderthk/src/utils/occupancy.ts
+++ roomfinderthk/src/utils/occupancy.ts
@@ -1,21 +1,34 @@
-export function getOccupancyColor(level: "empty" | "moderate" | "full") {
+export type OccupancyLevel = "empty" | "minimal" | "moderate" | "full";
+
+export function getOccupancyColor(level: OccupancyLevel) {
   switch (level) {
     case "empty":
       return "text-green-600";
-    case "moderate":
+    case "minimal":
       return "text-yellow-600";
+    case "moderate":
+      return "text-orange-600";
     case "full":
       return "text-red-600";
   }
-};
+}
 
-export function getOccupancyIcon(level: "empty" | "moderate" | "full") {
+export function getOccupancyIcon(level: OccupancyLevel) {
   switch (level) {
     case "empty":
+      return "○○○";
+    case "minimal":
       return "●○○";
     case "moderate":
       return "●●○";
     case "full":
       return "●●●";
   }
-};
+}
+
+export function getOccupancyLevel(checkins: number): OccupancyLevel {
+    if(checkins === 0) return "empty";
+    if(checkins <= 3) return "minimal";
+    if(checkins <= 10) return "moderate";
+    else return "full";
+}
