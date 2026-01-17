4f82522d3345df254f7bfb8f45e5e662827bb2cc
Kai Siepmann <kai_leo_theodor.siepmann@smail.th-koeln.de>
2026-01-17 12:18:39 +0100

    hotfixed sign in button, needs changes later

--- roomfinderthk/src/screens/LoginScreen.tsx
+++ roomfinderthk/src/screens/LoginScreen.tsx
@@ -86,6 +86,7 @@ export default function LoginScreen() {
                 type="submit"
                 className="w-full h-11"
                 disabled={isLoading}
+                onClick={login(email, password)}
               >
                 {isLoading ? (
                   <>
