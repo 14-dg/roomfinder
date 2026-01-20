c084f15e32c931e0ff5a68683c444245ad1a467b
Michael Liborius Beckmann <michael_liborius.beckmann@smail.th-koeln.de>
2026-01-20 19:24:10 +0100

    TimetableBuilder version more or less working version

--- roomfinderthk/src/models/Timetable.ts
+++ roomfinderthk/src/models/Timetable.ts
@@ -4,6 +4,9 @@
 //      - creating and loading a timetable like 'Stundenplan, TIN, 5. Semester, 2026'
 
 export interface Timetable {
+    courseOfStudy: string;
+    semester: string;
+    year: number;
     days: string[];
     events: Event[];
 }
--- roomfinderthk/src/screens/admin/TimetablesAdmin.tsx
+++ roomfinderthk/src/screens/admin/TimetablesAdmin.tsx
@@ -12,26 +12,8 @@ import {
   SelectValue,
 } from '@/components/ui/select';
 
-import TimetableBuilder from './timetablebuilder/TimetableBuilder';
-
-import { useData } from '@/contexts/DataContext';
-import { scheduler } from 'timers/promises';
-
-const schedule = {
-  days: ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag'],
-  times: ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00'],
-  events: [
-    { id: 1, name: 'Mathe', startTime: '08:00', endTime: '10:00', day: 'Montag' },
-    { id: 2, name: 'Englisch', startTime: '10:00', endTime: '12:00', day: 'Montag' },
-    { id: 3, name: 'Biologie', startTime: '13:00', endTime: '15:00', day: 'Dienstag' },
-    { id: 4, name: 'Chemie', startTime: '15:00', endTime: '17:00', day: 'Dienstag' },
-    { id: 5, name: 'Sport', startTime: '09:00', endTime: '11:00', day: 'Mittwoch' },
-    { id: 6, name: 'Kunst', startTime: '11:00', endTime: '13:00', day: 'Mittwoch' },
-    { id: 7, name: 'Geschichte', startTime: '08:00', endTime: '10:00', day: 'Donnerstag' },
-    { id: 8, name: 'Physik', startTime: '10:00', endTime: '12:00', day: 'Donnerstag' },
-    { id: 9, name : 'test', startTime: '08:00', endTime: '12:00', day: 'Montag' },
-  ],
-};
+// import TimetableBuilder from './timetablebuilder/TimetableBuilder';
+import TimetableBuilder from './timetablebuilder/TimetableBuilderNew';
 
 export default function TimetablesAdmin() {
   const [courseOfStudy, setCourseOfStudy] = useState('');
@@ -111,14 +93,14 @@ export default function TimetablesAdmin() {
         </Card>
       }
 
-      <Card className="p-6">
-        {/* PDF-Export Button */}
+      {/* PDF-Export Button */}
+      {/* <Card className="p-6">
           <Button
             className="w-full"
           >
             PDF-Export
           </Button>
-      </Card>
+      </Card> */}
 
     </div>
   );
