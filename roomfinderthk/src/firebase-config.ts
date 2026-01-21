44f3f5e9e5457a3913fe5ad4be0d5f40588bed8b
Daniel Graef <daniel.graef@smail.th-koeln.de>
2026-01-21 20:11:55 +0100

    added function to create a user without login in

--- roomfinderthk/src/firebase-config.ts
+++ roomfinderthk/src/firebase-config.ts
@@ -20,6 +20,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
+export { firebaseConfig };
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
