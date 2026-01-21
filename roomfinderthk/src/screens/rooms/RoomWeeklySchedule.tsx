fc02a8a86164d6278a11fd6b4e61abddb03f91d4
Kai Siepmann <kai_leo_theodor.siepmann@smail.th-koeln.de>
2026-01-21 22:59:01 +0100

    show lecturer name in room weekly schedule

--- roomfinderthk/src/screens/rooms/RoomWeeklySchedule.tsx
+++ roomfinderthk/src/screens/rooms/RoomWeeklySchedule.tsx
@@ -196,7 +196,7 @@ export function RoomWeeklySchedule({ lectures, bookings }: RoomWeeklySchedulePro
                         {item.title}
                       </h4>
                       <p className="text-sm text-gray-500 mt-1 flex items-center gap-1">
-                        {item.professor}
+                        {getLecturerName(item.professor)}
                       </p>
                     </div>
 
