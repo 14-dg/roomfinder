c55a767e4c329128319bde2a85d7c7b169496b51
Daniel Graef <daniel.graef@smail.th-koeln.de>
2026-01-24 15:34:44 +0100

    turned app into pwa

--- roomfinderthk/src/main.tsx
+++ roomfinderthk/src/main.tsx
@@ -1,8 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { registerSW } from 'virtual:pwa-register';
 import "./index.css";
 import App from "./App";
+
+// Registriert den Service Worker automatisch
+registerSW({ immediate: true });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
