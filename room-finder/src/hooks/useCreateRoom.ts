8f068c561cd82f2ceb8997913f452eeb48d99020
Kai Siepmann <kai_leo_theodor.siepmann@smail.th-koeln.de>
2026-01-25 17:15:53 +0100

    useCreateRoom hook modified

--- room-finder/src/hooks/useCreateRoom.ts
+++ room-finder/src/hooks/useCreateRoom.ts
@@ -1,11 +1,21 @@
 import { roomKeys } from "@/lib/queryKeys";
 import { createRoom } from "@/services/roomService";
-import { useMutation } from "@tanstack/react-query"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 
 export const useCreateRoom = () => {
+
+    const queryClient = useQueryClient();
+
     return useMutation({
-        mutationKey: roomKeys.all,
+        mutationKey: ['createRoom'],
         mutationFn: createRoom,
+        
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: roomKeys.lists()});
+        },
+
+        onError: (error) => {
+            console.error(error);
+        },
     });
-    
 }
