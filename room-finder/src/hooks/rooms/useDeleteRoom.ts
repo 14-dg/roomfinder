475f5b877bb86195041af7754ecdc5018bff9fc5
Kai Siepmann <kai_leo_theodor.siepmann@smail.th-koeln.de>
2026-01-25 17:47:25 +0100

    useDeleteRoom verbessert

--- room-finder/src/hooks/rooms/useDeleteRoom.ts
+++ room-finder/src/hooks/rooms/useDeleteRoom.ts
@@ -2,15 +2,15 @@ import { roomKeys } from "@/lib/queryKeys";
 import { deleteRoom } from "@/services/roomService";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-export const useDeleteRoom = (id: number) => {
+export const useDeleteRoom = () => {
     const queryClient = useQueryClient();
 
     return useMutation({
         mutationKey: ['deleteRoom'],
-        mutationFn: () => deleteRoom(id),
+        mutationFn: (id: number) => deleteRoom(id),
         
         onSuccess: () => {
-            queryClient.invalidateQueries({queryKey: roomKeys.details(id)});
+            queryClient.invalidateQueries({queryKey: roomKeys.list()});
         },
 
         onError: (error) => {
