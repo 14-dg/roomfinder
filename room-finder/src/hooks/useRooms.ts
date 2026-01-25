3b503c5d4a64f9d93396e9467d617bd9a2dc8470
Kai Siepmann <kai_leo_theodor.siepmann@smail.th-koeln.de>
2026-01-25 15:11:16 +0100

    useRooms uses the query key from the factory

--- room-finder/src/hooks/useRooms.ts
+++ room-finder/src/hooks/useRooms.ts
@@ -1,11 +1,13 @@
+import { roomKeys } from "@/lib/queryKeys";
 import { getAllRoomsWithDetails } from "@/services/roomService";
 import type { RoomWithDetails } from "@/types/models";
 import { useQuery } from "@tanstack/react-query"
 
 export const useRooms = () => {
     return useQuery<RoomWithDetails[], Error>({
-        queryKey: [],
+        queryKey: roomKeys.lists(),
         queryFn: getAllRoomsWithDetails,
+        //die gecashten Raum Daten sind nach 10 minuten stale/veraltet, das heißt sie werden neu geladen.
         staleTime: 1000 * 60 * 10
     })
 }
