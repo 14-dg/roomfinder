177b2421e251f3d36cf2e752283de85ebefa263d
Kai Siepmann <kai_leo_theodor.siepmann@smail.th-koeln.de>
2026-01-25 16:20:29 +0100

    added create, update, delete functions for rooms

--- room-finder/src/services/roomService.ts
+++ room-finder/src/services/roomService.ts
@@ -1,9 +1,26 @@
 import { supabase } from "@/lib/supabase";
-import type { RoomWithDetails } from "@/types/models";
+import type { NewRoom, Room, RoomWithDetails, UpdateRoom } from "@/types/models";
 
 export async function getAllRoomsWithDetails(): Promise<RoomWithDetails[]> {
     const {data, error} = await supabase.from("rooms").select("*, room_type:room_types(*), building:buildings(*, campus:campuses(*))");
     if(error) throw error;
     //Absicherung, data ist auf jeden fall vom Typ RoomWithDetails[]. Falls die Query null zurückgibt, wird [] verwendet.
     else return (data as RoomWithDetails[] || []);
+}
+
+export async function createRoom(newRoom: NewRoom): Promise<Room> {
+    const {data, error} = await supabase.from("rooms").insert(newRoom).select().single();
+    if(error) throw error;
+    return data as Room;
+}
+
+export async function updateRoom(id: number, updates: UpdateRoom): Promise<Room> {
+    const {data, error} = await supabase.from("rooms").update(updates).eq("id", id).select().single();
+    if(error) throw error;
+    return data as Room;
+}
+
+export async function deleteRoom(id: number) {
+    const {error} = await supabase.from("rooms").delete().eq("id",id);
+    if(error) throw error;
 }
