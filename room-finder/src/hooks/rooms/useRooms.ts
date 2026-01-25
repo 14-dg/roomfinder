37f740cdf97c0dda31906631150b6da84c5334fb
Kai Siepmann <kai_leo_theodor.siepmann@smail.th-koeln.de>
2026-01-25 17:48:26 +0100

    renamed back to useRooms

--- room-finder/src/hooks/rooms/useRooms.ts
+++ room-finder/src/hooks/rooms/useRooms.ts
@@ -5,7 +5,7 @@ import { useQuery } from "@tanstack/react-query"
 
 export const useRooms = () => {
     return useQuery<RoomWithDetails[], Error>({
-        queryKey: roomKeys.lists(),
+        queryKey: roomKeys.list(),
         queryFn: getAllRoomsWithDetails,
         //die gecashten Raum Daten sind nach 10 minuten stale/veraltet, das heißt sie werden neu geladen.
         staleTime: 1000 * 60 * 10
