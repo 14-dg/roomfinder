4830e37561168dae67fa5acf03af49f775f032f2
Kai Siepmann <kai_leo_theodor.siepmann@smail.th-koeln.de>
2026-01-25 12:49:35 +0100

    getAllRoomsWithDetails function edited so that it accomodates the changed model

--- room-finder/src/services/roomService.ts
+++ room-finder/src/services/roomService.ts
@@ -2,7 +2,7 @@ import { supabase } from "@/lib/supabase";
 import type { RoomWithDetails } from "@/types/models";
 
 export async function getAllRoomsWithDetails(): Promise<RoomWithDetails[]> {
-    const { data, error } = await supabase.from("rooms").select("*, room_type:room_types(*), building:buildings(*)");
+    const {data, error} = await supabase.from("rooms").select("*, room_type:room_types(*), building:buildings(*, campus:campuses(*))");
     if(error) throw error;
     //Absicherung, data ist auf jeden fall vom Typ RoomWithDetails[]. Falls die Query null zurückgibt, wird [] verwendet.
     else return (data as RoomWithDetails[] || []);
