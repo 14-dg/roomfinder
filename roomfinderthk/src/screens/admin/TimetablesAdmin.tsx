8f5f685b664c2c2410d64af7ef7742da6cb15b49
Daniel Graef <daniel.graef@smail.th-koeln.de>
2026-01-30 21:23:57 +0100

    renamed files that linked to deleted TimetableBuilder files

--- roomfinderthk/src/screens/admin/TimetablesAdmin.tsx
+++ roomfinderthk/src/screens/admin/TimetablesAdmin.tsx
@@ -10,7 +10,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select';
-import TimetableBuilder from './timetablebuilder/TimetableBuilderNew';
+import TimetableBuilder from './timetablebuilder/TimetableBuilder';
 
 export default function TimetablesAdmin() {
   const [courseOfStudy, setCourseOfStudy] = useState('');
