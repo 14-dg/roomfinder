8c32bb4bb2cb9953070235002a281b859ceb734c
Kai Siepmann <kai_leo_theodor.siepmann@smail.th-koeln.de>
2026-02-12 19:42:12 +0100

    added route protection to routing

--- room-finder/src/App.tsx
+++ room-finder/src/App.tsx
@@ -1,51 +1,57 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import './App.css'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import { Layout } from './components/Layout';
-import { RoomListScreen } from './screens/room/RoomListScreen';
-import { AdminScreen } from './screens/admin/AdminScreen';
-import { FavouritesScreen } from './screens/favourites/FavouritesScreen';
-import { LecturerScreen } from './screens/lecturer/LecturerScreen';
-import { ProfileScreen } from './screens/profile/ProfileScreen';
-import { BookingScreen } from './screens/bookings/BookingScreen';
-import { AuthProvider } from './contexts/AuthContext';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./App.css"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import { RoomListScreen } from "./screens/room/RoomListScreen";
+import { AdminScreen } from "./screens/admin/AdminScreen";
+import { FavouritesScreen } from "./screens/favourites/FavouritesScreen";
+import { LecturerScreen } from "./screens/lecturer/LecturerScreen";
+import { ProfileScreen } from "./screens/profile/ProfileScreen";
+import { BookingScreen } from "./screens/bookings/BookingScreen";
+import { AuthProvider } from "./contexts/AuthContext";
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import { LoginScreen } from "./screens/login/LoginScreen";
+
+const queryClient = new QueryClient();
 
 function App() {
 
-	const queryClient = new QueryClient();
 	return (
 		<QueryClientProvider client={queryClient}>
 			<AuthProvider>
 				<BrowserRouter>
 					<Routes>
+						
+						<Route path="/login" element={<LoginScreen />} />
+
 						<Route path="/" element={<Layout />}>
+
 							<Route index element={<Navigate to="/rooms" replace />} />
 
-							<Route path="rooms">
-								<Route index element={<RoomListScreen />} />
-								{/* <Route path="/:roomId" element={<RoomDetailScreen/>} /> */}
+							<Route element={<ProtectedRoute allowedRoles={["guest", "student", "lecturer", "admin"]} />}>
+								<Route path="rooms" element={<RoomListScreen />} />
+								{/* <Route path="rooms/:id" element={<RoomDetailScreen />} /> */}
+								<Route path="lecturers" element={<LecturerScreen />} />
+								<Route path="profile" element={<ProfileScreen />} />
 							</Route>
 
-							<Route path="favorites">
-								<Route index element={<FavouritesScreen />} />
+							<Route element={<ProtectedRoute allowedRoles={["student", "lecturer", "admin"]} />}>
+								<Route path="favorites" element={<FavouritesScreen />} />
 							</Route>
 
-							<Route path="lecturers">
-								<Route index element={<LecturerScreen />} />
+							<Route element={<ProtectedRoute allowedRoles={["lecturer", "admin"]} />}>
+								<Route path="bookings" element={<BookingScreen />} />
+							</Route>
+							
+							<Route element={<ProtectedRoute allowedRoles={["admin"]} />}>
+								<Route path="admin" element={<AdminScreen />} />
 							</Route>
 
-							<Route path="bookings">
-								<Route index element={<BookingScreen />} />
-							</Route>
-
-							<Route path="profile">
-								<Route index element={<ProfileScreen />} />
-							</Route>
-
-							<Route path="admin">
-								<Route index element={<AdminScreen />} />
-							</Route>
 						</Route>
+
+						{/* Fallback für falsche URLs (404) */}
+						<Route path="*" element={<Navigate to="/rooms" replace />} />
+
 					</Routes>
 				</BrowserRouter>
 			</AuthProvider>
