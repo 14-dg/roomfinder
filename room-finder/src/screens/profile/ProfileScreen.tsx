5c22f84d75947b59049e3efdb15eb74160950fd2
Kai Siepmann <kai_leo_theodor.siepmann@smail.th-koeln.de>
2026-02-14 15:53:22 +0100

    profil screen when not logged in created

--- room-finder/src/screens/profile/ProfileScreen.tsx
+++ room-finder/src/screens/profile/ProfileScreen.tsx
@@ -1,7 +1,44 @@
+import { useAuth } from "@/hooks/auth/useAuth";
+import { LogIn } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+
 export function ProfileScreen() {
-    return(
+
+    const { isAuthenticated, signOut } = useAuth();
+
+    const navigate = useNavigate();
+
+    const handleToLogin = () => {
+        navigate("/login");
+    }
+
+    if (isAuthenticated) return (
         <div>
-            Profil
+            <button
+                onClick={signOut}
+            >
+                {"Abmelden"}
+            </button>
+        </div>
+    );
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-[80vh] px-6 text-center animate-in fade-in duration-500">
+            <div className="bg-blue-50 p-6 rounded-full mb-6">
+                <LogIn size={48} className="text-blue-600" />
+            </div>
+            <h2 className="text-2xl font-bold text-gray-900 mb-3">
+                {"Nicht angemeldet."}
+            </h2>
+            <p className="max-w-sm mb-8 leading-relaxed">
+                {"Melde dich an, um deinen persönlichen Stundenplan zu verwenden und Räume als Favoriten zu speichern."}
+            </p>
+            <button
+                className="w-full max-w-sm bg-orange-600 text-white py-3.5 rounded-xl font-semibold shadow-lg shadow-blue-200 hover:bg-blue-700 active:scale-95 transition-all"
+                onClick={handleToLogin}
+            >
+                {"zum Login"}
+            </button>
         </div>
     );
 }
